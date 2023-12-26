f57276856a0b03aac0788c1a0096300da7b4c834
sudharsansubash2002 <sudharsan@bosonlabs.net>
2023-12-26 11:44:20 +0530

    Joker UI bugFix part 2

--- src/components/DashboardNew/Faucet.js
+++ src/components/DashboardNew/Faucet.js
@@ -149,7 +149,7 @@ const algodClientGet = new algosdk.Algodv2('', node['algodclient'], '');
      const toastDiv = (txId) =>
     (
         <div>
-            <p> Transaction is successful &nbsp;<a style={{color:'#919cff'}} href={txId} target="_blank" rel="noreferrer"><br/><p style={{fontWeight: 'bold'}}>View in Algo Explorer <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <p> Transaction is successful &nbsp;<a style={{color:'#919cff'}} href={txId} target="_blank" rel="noreferrer"><br/><p style={{fontWeight: 'bold'}}>View in Sepolia Explorer <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
      <path d="M11.7176 3.97604L1.69366 14L0.046875 12.3532L10.0697 2.32926H1.23596V0H14.0469V12.8109H11.7176V3.97604Z" fill="#919cff"/>
      </svg></p></a></p> 
         </div>
